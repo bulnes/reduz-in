66f8fcb3066eeb93930a3e3f939f08d2009e7b0f
Bruno Bulnes <bulnes.bruno@gmail.com>
2025-06-22 01:40:57 -0300

    feat: install boostrap and react-bootstrap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import { Col, Container, Row } from "react-bootstrap";
+
 export default function Home() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <Container>
+      <Row>
+        <Col>
+          <h1>Hello World</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>1 of 2</Col>
+        <Col>2 of 2</Col>
+      </Row>
+      <Row>
+        <Col>1 of 3</Col>
+        <Col>2 of 3</Col>
+        <Col>3 of 3</Col>
+      </Row>
+    </Container>
   );
 }
