83bd162035adc41e25841f5e9e67177c3e903258
Bruno Bulnes <bulnes.bruno@gmail.com>
2025-08-24 23:33:36 -0300

    feat: add global 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div>
+      <div className="custom-bg text-dark">
+        <div className="d-flex align-items-center justify-content-center min-vh-100 px-2">
+          <div className="text-center">
+            <h1 className="display-1 fw-bold">404</h1>
+            <p className="fs-2 fw-medium mt-4">Oops! Page not found</p>
+            <p className="mt-4 mb-5">
+              The page you&#39;re looking for doesn&#39;t exist or has been
+              moved.
+            </p>
+
+            <Link
+              href="/"
+              className="btn btn-primary fw-semibold rounded-pill px-4 py-2 custom-btn"
+            >
+              Go Home
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
