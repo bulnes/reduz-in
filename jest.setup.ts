e632eaad99d0ee3ca9c6a58fc93184ce718e0ac2
Bruno Bulnes <bulnes.bruno@gmail.com>
2026-04-28 01:17:24 -0300

    test: suppress console output during jest test execution

--- jest.setup.ts
+++ jest.setup.ts
@@ -12,6 +12,13 @@ if (typeof global.Request === 'undefined') {
   global.fetch = globalThis.fetch
 }
 
+// Suppress console output during tests
+jest.spyOn(console, 'log').mockImplementation(() => {})
+jest.spyOn(console, 'error').mockImplementation(() => {})
+jest.spyOn(console, 'warn').mockImplementation(() => {})
+jest.spyOn(console, 'info').mockImplementation(() => {})
+jest.spyOn(console, 'debug').mockImplementation(() => {})
+
 jest.mock('next/cache', () => ({
   revalidatePath: jest.fn(),
 }))
